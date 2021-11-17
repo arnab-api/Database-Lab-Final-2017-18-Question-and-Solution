f1115f6f1d91fb73d146e16af3228c2f040ecc23
Arnab Sen Sharma <arnab.api@gmail.com>
2021-11-17 20:39:15 +0600

    Update sql_b.sql

--- sql_b.sql
+++ sql_b.sql
@@ -1,3 +1,3 @@
 -- Show the mean, minimum and maximum marks obtained by the students in the course with code CSE333.
 
-select avg(marks), min(marks), max(marks) from marks;
+select avg(marks), min(marks), max(marks) from marks where course_code="CSE333";
